d8d4779c60a7a840640ffb8d20a1a944d3018b7f
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-09-20 10:01:26 +0000

    update dependencies and implement random number generation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     // taking user input
     println!("Please input your guess: ");
     let mut guess = String::new();
-
+    
     io::stdin().read_line(&mut guess).expect("Failed to read user input");
-
+    
     println!("You guessed {}", guess);
-
+    
     // generating random number b/w 1-100
+    let secret_number = rand::random_range(1..=100);
+
+    
     
 }
