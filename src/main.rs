6286323eaa443a518efb3a7054f9d13a73bf1ba7
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-09-20 09:21:16 +0000

    taking user input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io;
 
 fn main() {
+    // taking user input
     println!("Please input your guess: ");
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read user input");
 
-    println!("You guessed {guess}");
+    println!("You guessed {}", guess);
+
+    // generating random number b/w 1-100
     
 }
